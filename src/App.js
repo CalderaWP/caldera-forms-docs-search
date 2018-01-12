9a1f2cb5a5270d189223cede78f9fbbcdc6d3d0e
Josh Pollock <josh@calderawp.com>
2018-01-11 21:18:18 -0500

    just show doc search

--- src/App.js
+++ src/App.js
@@ -9,23 +9,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          <Navbar>
-              <Navbar.Header>
-                  <Navbar.Brand>
-                      <a href="#home">
-                          <img src={logo} alt="logo" style={{width:"25px"}}/>
-                      </a>
-                  </Navbar.Brand>
-              </Navbar.Header>
-              <Nav>
-                  <NavItem eventKey={1} href="https://calderaforms.com">
-                      Caldera Forms
-                  </NavItem>
-                  <NavItem eventKey={2} href="https://calderaforms.com/support">
-                      Support
-                  </NavItem>
-              </Nav>
-          </Navbar>
 
           <DocSearch/>
 
