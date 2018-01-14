fc460a1f4a7bf5a529b8efd2fd383f77b8d3d960
Josh Pollock <josh@calderawp.com>
2018-01-13 22:06:43 -0500

    clean up top bar

--- src/Components/TopBar.js
+++ src/Components/TopBar.js
@@ -1,59 +1,66 @@
 import React from 'react';
-import { Navbar } from 'react-bootstrap';
+import {Navbar, Nav, NavItem, Glyphicon} from 'react-bootstrap';
 
 export class TopBar extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            isOpen:this.props.isOpen
+            isOpen: this.props.isOpen
         };
 
         this.text = this.text.bind(this);
         this.title = this.title.bind(this);
-        this.iconClass = this.iconClass.bind(this);
+        this.icon = this.icon.bind(this);
     }
 
-    text(){
-        if( this.props.isOpen ){
-            return 'Close'
+    text() {
+        if (this.props.isOpen) {
+            return 'Close Search Panel'
         }
-        return 'Open'
+        return 'Open Search Panel'
     }
 
-    title(){
-        if( this.props.isOpen ){
+    title() {
+        if (this.props.isOpen) {
             return 'Close Search Panel';
         }
         return 'Open Search Panel';
     }
 
-    iconClass(){
-        if( this.props.isOpen ){
-            return 'a fa-times-circle-o';
+    icon() {
+        if (this.props.isOpen) {
+            return 'remove-circle';
         }
-        return 'a fa-search-plus';
+        return 'search';
 
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <div>
                 <Navbar
                     inverse
                     className="affix"
                 >
-                    <button
-                        style={{width:'100%'}}
-                        title={this.title()}
-                        onClick={this.props.toggleOpen}
-                    >
 
-                        <i
-                            className={this.iconClass()}
-                            aria-hidden="true"
-                        />
-                            {this.text()}
-                    </button>
+                    <Nav>
+                        <NavItem
+                            className={'cf-doc-search-sidebar-toggle'}
+                            eventKey={1}
+                            href="#"
+                            title={this.title()}
+                            onClick={this.props.toggleOpen}
+                        >
+                            <Glyphicon
+                                glyph={this.icon()}
+                            />
+                            <span
+                                className={'description'}
+                            >
+                                {this.text()}
+                            </span>
+                        </NavItem>
+                    </Nav>
                 </Navbar>
             </div>
         )
