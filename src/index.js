b4c33812f978dd4f78523b16ba6c9bda8875b044
Josh Pollock <josh@calderawp.com>
2018-01-13 21:46:08 -0500

    set api root from dom if possible

--- src/index.js
+++ src/index.js
@@ -5,5 +5,7 @@ import './index.css';
 import App from './App';
 //import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App apiRoot='https://calderaforms.com/wp-json'/>, document.getElementById('cf-docs-search-app'));
+const el = document.getElementById('cf-docs-search-app');
+const apiRoot = el.getAttribute( 'data-api-root');
+ReactDOM.render(<App apiRoot={apiRoot} />, el );
 //registerServiceWorker();
