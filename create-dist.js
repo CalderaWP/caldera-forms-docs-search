293d46da162e8c0f440bf69ebdbcd0918889811f
Josh Pollock <josh@calderawp.com>
2018-01-19 14:46:44 -0500

    change dist for css

--- create-dist.js
+++ create-dist.js
@@ -16,20 +16,25 @@ const options = {
     rename:(filePath) => {
         let dotSplit = filePath.split('.');
         if( 'css/main' === dotSplit[0] || 'js/main' ===dotSplit[0]){
-            filePath = filePath.replace( dotSplit[1] + '.', '' );
+            filePath = filePath.replace( `${dotSplit[1]}.`, '' );
         }
         return filePath;
     },
 };
 
 const buildPath = `./dist`;
-copy('./build/static', buildPath, options)
+const staticPath = './build/static';
+copy(staticPath, buildPath, options)
     .then(function(results) {
 
     })
     .catch(function(error) {
-        console.error('Copy failed: ' + error);
+        console.error('Copy failed (dist):  ' + error);
     });
 
+copy(staticPath, `${staticPath}/dist`, options)
+    .catch(function(error) {
+        console.error('Copy failed (static dist): ' + error);
+    });
 
 
